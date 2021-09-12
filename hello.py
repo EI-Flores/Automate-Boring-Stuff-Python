87687f5e3c1f18bc16c257e39a872bb54392eaf1
EI-Flores <ing.flores.rivera@gmail.com>
2021-09-12 18:42:26 -0500

    Hello

---
+++ hello.py
@@ -0,0 +1,14 @@
+# This soft ask your name
+
+print("\n\nHello World!")
+print("What is your name")
+my_name = input()
+
+print("\nGreat! Nice to meet you" + my_name)
+print("The length of your name is: ")
+print(len(my_name))
+
+print("\nWhat is your age?")
+my_age = input()
+
+print("\nYou will be " + str(int(my_age) + 1) + " in a year.\n\n")
